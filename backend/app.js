2a8322b37ceefca117ee5fb9db854fed4439c740
Rachit Patel <rachitp509@gmail.com>
2022-08-15 20:29:57 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,10 @@ const user = require("./routes/Userroute");
 app.use("/api/v1", post);
 app.use("/api/v1", user);
 
-app.use(express.static(path.join(__dirname, "../frontend/public")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/public/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
 module.exports = app;
